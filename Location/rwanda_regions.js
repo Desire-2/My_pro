ef2e4b2761e09515071e2b3b35ebe9c5f88bd615
Desire-2 <bikorimanadesire5@gmail.com>
2023-10-01 22:38:20 +0200

    House Hunter

--- Location/rwanda_regions.js
+++ Location/rwanda_regions.js
@@ -1,73 +1,85 @@
-// Function to create buttons
-function createButtons(data, container, level, filter) {
-    container.innerHTML = ""; // Clear existing buttons
-  
-    for (const name in data) {
-      const button = document.createElement("button");
-      button.textContent = name;
-      button.addEventListener("click", () => {
-        if (level === "Houses") {
-          // Display houses when at the village level
-          displayHouses(data[name]);
-        } else {
-          // Handle button click for this level
-          const subLevel = data[name];
-          createButtons(subLevel, container, name, filter);
-        }
-      });
-  
-      // Apply filter if specified
-      if (filter === "Houses") {
-        const houses = data[name];
-        const houseCount = houses.length;
-        button.textContent = `${name} (${houseCount} Houses)`;
-      }
-  
-      container.appendChild(button);
-    }
+// Assuming you have an element with the ID 'provinceContainer' in your HTML
+const provinceContainer = document.getElementById("provinceContainer");
+
+// Function to create buttons for provinces
+function createProvinceButtons(data) {
+  for (const region in data) {
+    const regionButton = document.createElement("button");
+    regionButton.textContent = region;
+    regionButton.addEventListener("click", () => {
+      displayDistricts(data[region]);
+    });
+    provinceContainer.appendChild(regionButton);
   }
-  
-  // Function to display houses
-  function displayHouses(houses) {
-    const houseContainer = document.getElementById("houseContainer");
-    houseContainer.innerHTML = "";
-  
-    for (const house of houses) {
-      const houseElement = document.createElement("div");
-      houseElement.textContent = house;
-      houseContainer.appendChild(houseElement);
-    }
+}
+
+// Function to display districts for a selected province
+function displayDistricts(provinceData) {
+  const districtContainer = document.getElementById("districtContainer");
+  districtContainer.innerHTML = "";
+
+  for (const district in provinceData) {
+    const districtButton = document.createElement("button");
+    districtButton.textContent = district;
+    districtButton.addEventListener("click", () => {
+      displaySectors(provinceData[district]);
+    });
+    districtContainer.appendChild(districtButton);
   }
-  
-  // Function to load data from data.json
-  function loadData() {
-    fetch("./data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const provinceContainer = document.getElementById("provinceContainer");
-        createButtons(data, provinceContainer, "Rwanda");
-      })
-      .catch((error) => {
-        console.error("Error loading data:", error);
-      });
+}
+
+// Function to display sectors for a selected district
+function displaySectors(districtData) {
+  const sectorContainer = document.getElementById("sectorContainer");
+  sectorContainer.innerHTML = "";
+
+  for (const sector in districtData) {
+    const sectorButton = document.createElement("button");
+    sectorButton.textContent = sector;
+    sectorButton.addEventListener("click", () => {
+      displayCells(districtData[sector]);
+    });
+    sectorContainer.appendChild(sectorButton);
   }
-  
-  // Handle search input
-  const searchInput = document.getElementById("searchInput");
-  searchInput.addEventListener("input", () => {
-    const searchTerm = searchInput.value.toLowerCase();
-    // Implement search logic here
-    // You can filter and update the displayed buttons based on the search term
+}
+
+// Function to display cells for a selected sector
+function displayCells(sectorData) {
+  const cellContainer = document.getElementById("cellContainer");
+  cellContainer.innerHTML = "";
+
+  for (const cell in sectorData) {
+    const cellButton = document.createElement("button");
+    cellButton.textContent = cell;
+    cellButton.addEventListener("click", () => {
+      displayVillages(sectorData[cell]);
+    });
+    cellContainer.appendChild(cellButton);
+  }
+}
+
+// Function to display villages and houses for a selected cell
+function displayVillages(cellData) {
+  const villageContainer = document.getElementById("villageContainer");
+  villageContainer.innerHTML = "";
+
+  for (const village in cellData) {
+    const villageButton = document.createElement("button");
+    villageButton.textContent = village;
+    villageButton.addEventListener("click", () => {
+      // Implement logic to display houses for the selected village
+      // You can use cellData[village] to access the list of houses in the village
+    });
+    villageContainer.appendChild(villageButton);
+  }
+}
+
+// Load data from data.json
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    createProvinceButtons(data);
+  })
+  .catch((error) => {
+    console.error("Error loading data:", error);
   });
-  
-  // Handle filter selection
-  const filterSelect = document.getElementById("filterSelect");
-  filterSelect.addEventListener("change", () => {
-    const filterValue = filterSelect.value;
-    const provinceContainer = document.getElementById("provinceContainer");
-    createButtons(data, provinceContainer, "Rwanda", filterValue);
-  });
-  
-  // Call the loadData function when the page loads
-  loadData();
-  
